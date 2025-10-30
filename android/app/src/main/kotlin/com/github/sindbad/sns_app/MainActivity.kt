71d3c98dd8646ec44fe24d979103c67f227ef69b
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2025-10-31 00:36:18 +0100

    Create android channel for accessing satellites position data

--- android/app/src/main/kotlin/com/github/sindbad/sns_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/github/sindbad/sns_app/MainActivity.kt
@@ -1,39 +1,85 @@
 package com.github.sindbad.sns_app
 
 import android.content.Context
+import android.location.GnssStatus
+import android.location.LocationManager
 import android.os.BatteryManager
 import android.os.Handler
 import android.os.Looper
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.EventChannel
 
 class MainActivity : FlutterActivity() {
-    private val eventChannelName = "com.github.sindbad/battery"
-    private var handler: Handler? = null
-    private var runnable: Runnable? = null
+    private val batteryEventChannel = "com.github.sindbad/battery"
+    private val satelliteEventChannel = "com.github.sindbad/satellite"
+    private var batteryHandler: Handler? = null
+    private var batteryRunnable: Runnable? = null
+    private var satelliteHandler: Handler? = null
+    private var satelliteRunnable: Runnable? = null
 
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        EventChannel(flutterEngine.dartExecutor.binaryMessenger, eventChannelName).setStreamHandler(
+
+        // Battery channel
+        EventChannel(flutterEngine.dartExecutor.binaryMessenger, batteryEventChannel).setStreamHandler(
             object : EventChannel.StreamHandler {
                 override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
                     val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
-                    handler = Handler(Looper.getMainLooper())
-                    runnable = object : Runnable {
+                    batteryHandler = Handler(Looper.getMainLooper())
+                    batteryRunnable = object : Runnable {
                         override fun run() {
                             val current = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)
                             events?.success(current)
-                            handler?.postDelayed(this, 1000)
+                            batteryHandler?.postDelayed(this, 1000)
                         }
                     }
-                    handler?.post(runnable!!)
+                    batteryHandler?.post(batteryRunnable!!)
                 }
 
                 override fun onCancel(arguments: Any?) {
-                    handler?.removeCallbacks(runnable!!)
-                    runnable = null
-                    handler = null
+                    batteryHandler?.removeCallbacks(batteryRunnable!!)
+                    batteryRunnable = null
+                    batteryHandler = null
+                }
+            }
+        )
+
+        // Satellite channel
+        EventChannel(flutterEngine.dartExecutor.binaryMessenger, satelliteEventChannel).setStreamHandler(
+            object : EventChannel.StreamHandler {
+                private var gnssStatusCallback: GnssStatus.Callback? = null
+
+                override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+                    val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                    gnssStatusCallback = object : GnssStatus.Callback() {
+                        override fun onSatelliteStatusChanged(status: GnssStatus) {
+                            val satellites = mutableListOf<Map<String, Any>>()
+                            for (i in 0 until status.satelliteCount) {
+                                satellites.add(
+                                    mapOf(
+                                        "azimuth" to status.getAzimuthDegrees(i),
+                                        "elevation" to status.getElevationDegrees(i)
+                                    )
+                                )
+                            }
+                            events?.success(satellites)
+                        }
+                    }
+                    try {
+                        locationManager.registerGnssStatusCallback(gnssStatusCallback!!, Handler(Looper.getMainLooper()))
+                    } catch (e: SecurityException) {
+                        events?.error("PERMISSION_DENIED", "Location permission not granted", null)
+                    }
+                }
+
+                override fun onCancel(arguments: Any?) {
+                    val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                    if (gnssStatusCallback != null) {
+                        locationManager.unregisterGnssStatusCallback(gnssStatusCallback!!)
+                        gnssStatusCallback = null
+                    }
                 }
             }
         )
