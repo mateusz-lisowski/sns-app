b4484cb30d497d75b749b10f98627a35198e893e
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2025-10-30 23:24:12 +0100

    Remove unnecessary logging from android channel function

--- android/app/src/main/kotlin/com/github/sindbad/sns_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/github/sindbad/sns_app/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.os.BatteryManager
 import android.os.Handler
 import android.os.Looper
-import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.EventChannel
@@ -13,20 +12,17 @@ class MainActivity : FlutterActivity() {
     private val eventChannelName = "com.github.sindbad/battery"
     private var handler: Handler? = null
     private var runnable: Runnable? = null
-    private val TAG = "MainActivity"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         EventChannel(flutterEngine.dartExecutor.binaryMessenger, eventChannelName).setStreamHandler(
             object : EventChannel.StreamHandler {
                 override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
-                    Log.d(TAG, "onListen called: Starting battery stream.")
                     val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
                     handler = Handler(Looper.getMainLooper())
                     runnable = object : Runnable {
                         override fun run() {
                             val current = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CURRENT_NOW)
-                            Log.d(TAG, "Polling for battery current: $current µA")
                             events?.success(current)
                             handler?.postDelayed(this, 1000)
                         }
@@ -35,7 +31,6 @@ class MainActivity : FlutterActivity() {
                 }
 
                 override fun onCancel(arguments: Any?) {
-                    Log.d(TAG, "onCancel called: Stopping battery stream.")
                     handler?.removeCallbacks(runnable!!)
                     runnable = null
                     handler = null
