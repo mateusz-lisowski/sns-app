abae04c68dd4fabf907b51ca36ffd39d75f03ae6
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2025-10-31 13:56:34 +0100

    Create path tracking screen

---
+++ lib/screens/path_tracking_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sns_app/controllers/location_controller.dart';
+import 'package:sns_app/widgets/path_painter.dart';
+
+class PathTrackingScreen extends StatelessWidget {
+  final LocationController locationController = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Path Tracking'),
+      ),
+      body: Column(
+        children: [
+          Obx(() => SwitchListTile(
+            title: Text('Track Path'),
+            value: locationController.isLocationEnabled.value,
+            onChanged: (bool value) {
+              locationController.toggleLocationServices();
+            },
+          )),
+          Expanded(
+            child: Obx(() => CustomPaint(
+              painter: PathPainter(path: locationController.path.toList()),
+              child: Container(),
+            )),
+          ),
+        ],
+      ),
+    );
+  }
+}
